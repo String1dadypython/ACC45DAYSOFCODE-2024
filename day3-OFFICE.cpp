0772fe816d4e339655e1069470eb5809f7101eca
String1dadypython <145442638+String1dadypython@users.noreply.github.com>
2024-09-30 15:46:40 +0530

    Add files via upload

---
+++ day3-OFFICE.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,P;
+	cin>>T;
+	int x,y;
+	for(int i=0;i<T;i++){
+	    cin>>x>>y;
+	    P=(4*x)+y;
+	    cout<<P<<endl;
+	    x=0;
+	    y=0;
+	}
+
+}
