5121b726754e4b729462bc221c21ec2c7afdbc14
String1dadypython <145442638+String1dadypython@users.noreply.github.com>
2024-10-01 18:55:34 +0530

    Add files via upload

---
+++ day4-THREETOPICS.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int A,B,C,X;
+	cin>>A>>B>>C>>X;
+	if(A==X||B==X||C==X){
+	    cout<<"yes"<<endl;
+	    
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+
+}
