20b1e394f8e414ebe6db082cd854c82454c7b5b6
String1dadypython <145442638+String1dadypython@users.noreply.github.com>
2024-10-04 14:35:03 +0530

    Add files via upload

---
+++ day7-JASSIGNMENTS.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	for(int i=0;i<t;i++){
+	    cin>>x;
+	    if((x+3)<=10){
+	        cout<<"yes"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	
+
+}
