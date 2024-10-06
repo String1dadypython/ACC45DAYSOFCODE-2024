3dfae03b681a8d926e042cd2af551307ab1da140
String1dadypython <145442638+String1dadypython@users.noreply.github.com>
2024-10-06 23:51:22 +0530

    Add files via upload

---
+++ day9-ADD12GAME.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,n,x=0;
+	cin>>t;
+	while(t--){
+	    cin>>n;
+	    if(n==1){
+	        cout<<"ALICE"<<endl;
+	        continue;
+	        
+	        
+	    }
+	    if(n==2){
+	        cout<<"BOB"<<endl;
+	        continue;
+	        
+	    }
+	    else{
+	        cout<<"BOB"<<endl;
+	        continue;
+	        
+	    }
+	    
+	    
+	        
+	    
+	    
+	}
+
+}
