06d32833e3b9cc03b93884c34dec19cf1a232c29
String1dadypython <145442638+String1dadypython@users.noreply.github.com>
2024-10-10 23:40:59 +0530

    Add files via upload

---
+++ day13-EXPERT.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	// your code goes here
+	int t;
+	cin>>t;
+	float x,y;
+	for(int i=0;i<t;i++){
+	    cin>>x>>y;
+	    if(y>=(x/2.0)){
+	        cout<<"YES"<<endl;
+	        
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
