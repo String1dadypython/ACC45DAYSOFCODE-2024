6cd45ed03baaec178583fd3197ce4f65e6086068
String1dadypython <145442638+String1dadypython@users.noreply.github.com>
2024-10-22 11:09:36 +0530

    Add files via upload

---
+++ Day20-CHEFGAMES.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,a,b,c,d;
+	cin>>n;
+	for(int i=0;i<n;i++){
+	    cin>>a>>b>>c>>d;
+	    if(a==0&&b==0&&c==0&&d==0){
+	        cout<<"IN"<<endl;
+	    }
+	    else{
+	        cout<<"OUT"<<endl;
+	    }
+	}
+
+}
