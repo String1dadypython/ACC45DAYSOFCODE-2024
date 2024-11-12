b635dcd5fa6f5f34a342cd4707893a0f4009d82b
String1dadypython <145442638+String1dadypython@users.noreply.github.com>
2024-11-12 15:54:24 +0530

    Add files via upload

---
+++ Day41-BATH.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	int x,y;
+	cin>>t;
+	for(int i=0;i<t;i++){
+	    cin>>x>>y;
+	    cout<<(x/(2*y))<<endl;
+	}
+
+}
